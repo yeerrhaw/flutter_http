e6b39dbde036604823e1b2e97848fda663b8fbad
yerrhaw <maevenr81@gmail.com>
2024-12-14 10:09:46 +0800

    alldata

--- lib/alldata.dart
+++ lib/alldata.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 
 class AllData extends StatefulWidget {
   const AllData({super.key});
---
+++ lib/users.dart
@@ -0,0 +1,15 @@
+class Users {
+  final int id;
+  final String first_name;
+  final String last_name;
+  final String email;
+  final String avatar;
+
+  Users({
+    required this.id,
+    required this.email,
+    required this.first_name,
+    required this.last_name,
+    required this.avatar,
+  });
+}
